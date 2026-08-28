6d016de4a8dc87717fa91b3890b8e62f78f12d91
ReidS28 <strawj3434@gmail.com>
2026-08-28 18:27:21 -0400

    svelte again

--- src/pages/Contact.vue
+++
@@ -1,7 +0,0 @@
-<template>
-	<div>
-		<h1>Contact Page</h1>
-		<p>You may be able to find the imformation you want on the home page <router-link to="/">here</router-link>.</p>
-		<p>Good Luck</p>
-	</div>
-</template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,7 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    host: "0.0.0.0",
-    port: 5000,
-    allowedHosts: true,
-    proxy: {
-      // PROTOMAPS PROXY RULE
-      "/pmtiles": {
-        target: "https://demo-bucket.protomaps.com",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/pmtiles/, ""),
-      },
-      // OVERTURE MAPS PROXY RULE
-      "/overture": {
-        target: "https://overturemaps-tiles-us-west-2-beta.s3.amazonaws.com",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/overture/, ""),
-      },
-    },
-  },
-  optimizeDeps: {
-    include: ["maplibre-gl"],
-  },
-});
+  plugins: [svelte()],
+})
